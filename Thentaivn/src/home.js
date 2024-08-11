291d4e51133935692fc78ef70878407ee1132ebd
spirittomb991 <tuansaker991@gmail.com>
2024-08-12 00:22:33 +0700

    2.0

--- Thentaivn/src/home.js
+++ Thentaivn/src/home.js
@@ -4,6 +4,7 @@ function execute() {
         {title: "Truyện mới", input: BASE_URL + "/danh-sach.html", script: "gen.js"},
         {title: "Chương mới", input: BASE_URL + "/chap-moi.html", script: "gen.js"},
         {title: "Full màu", input: BASE_URL + "/the-loai-37-full_color.html", script: "gen.js"},
+        {title: "Series", input: BASE_URL + "/the-loai-87-series.html", script: "gen.js"},
         {title: "Không che", input: BASE_URL + "/the-loai-99-khong_che.html", script: "gen.js"}
     ]);
 }
