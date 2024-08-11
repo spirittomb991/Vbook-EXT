472b3a582f02e589581c6b60ea707ef45ce73778
spirittomb991 <tuansaker991@gmail.com>
2024-08-12 00:42:24 +0700

    2.1

--- Tlxhentai/src/home.js
+++ Tlxhentai/src/home.js
@@ -2,5 +2,7 @@ function execute() {
     return Response.success([
         {title: "Cập Nhật", input: "-updated_at", script: "gen.js"},
         {title: "Xem Nhiều", input: "-views", script: "gen.js"},
+        {title: "Mới nhất", input: "-created_at", script: "gen.js"},
+        {title: "Cũ nhất", input: "created_at", script: "gen.js"},
     ]);
 }
