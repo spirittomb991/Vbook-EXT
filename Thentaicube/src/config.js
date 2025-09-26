ff17dedfcaaa1d37948999509ac58cdc231bc7e2
tuansaker991 <tuansaker991@gmail.com>
2025-09-26 11:17:46 +0700

    cube7

--- Thentaicube/src/config.js
+++ Thentaicube/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://hentaicb.help";
+let BASE_URL = "https://hentaicube.xyz";
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
--- Thentaicube/src/home.js
+++ Thentaicube/src/home.js
@@ -1,10 +1,10 @@
 function execute() {
     return Response.success([
-        {title: "Cập nhật", input: "https://hentaicb.help", script: "gen.js"},
-        {title: "Truyện Mới", input: "https://hentaicb.help/read/?m_orderby=new-manga", script: "gen.js"},
-        {title: "Truyện Update", input: "https://hentaicb.help/read/?m_orderby=latest", script: "gen.js"},
-        {title: "Trending", input: "https://hentaicb.help/read/?m_orderby=trending", script: "gen.js"},
-        {title: "Đọc nhiều", input: "https://hentaicb.help/read/?m_orderby=views", script: "gen.js"},
-        {title: "Hoàn Thành", input: "https://hentaicb.help/?s=&post_type=wp-manga&genre%5B%5D=series&op=1&author=&status%5B%5D=end", script: "gen.js"},
+        {title: "Cập nhật", input: "https://hentaicube.xyz", script: "gen.js"},
+        {title: "Truyện Mới", input: "https://hentaicube.xyz/read/?m_orderby=new-manga", script: "gen.js"},
+        {title: "Truyện Update", input: "https://hentaicube.xyz/read/?m_orderby=latest", script: "gen.js"},
+        {title: "Trending", input: "https://hentaicube.xyz/read/?m_orderby=trending", script: "gen.js"},
+        {title: "Đọc nhiều", input: "https://hentaicube.xyz/read/?m_orderby=views", script: "gen.js"},
+        {title: "Hoàn Thành", input: "https://hentaicube.xyz/?s=&post_type=wp-manga&genre%5B%5D=series&op=1&author=&status%5B%5D=end", script: "gen.js"},
     ]);
 }
