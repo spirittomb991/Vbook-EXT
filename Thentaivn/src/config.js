ee8eb0b3fa4760e23e3297028f61f27a98b56cb2
Tuan <tuansaker991@gmail.com>
2026-05-12 15:38:40 +0700

    120526

--- Thentaivn/src/config.js
+++ Thentaivn/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = 'https://www.hentaivnx.com';
+let BASE_URL = 'https://www.hentaivnx.vip';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
--- Thentaivn/src/home.js
+++ Thentaivn/src/home.js
@@ -1,10 +1,10 @@
 load('config.js');
 function execute() {
     return Response.success([
-        {title: "Trang chủ", input: "https://www.hentaivnx.com", script: "gen.js"},
-        {title: "Truyện mới", input: "https://www.hentaivnx.com/tim-truyen", script: "gen.js"},
-        {title: "Không che", input: "https://www.hentaivnx.com/tim-truyen/khong-che", script: "gen.js"},
-        {title: "Truyện màu", input: "https://www.hentaivnx.com/tim-truyen/truyen-mau", script: "gen.js"},
-        {title: "Series", input: "https://www.hentaivnx.com/tim-truyen/series", script: "gen.js"}
+        {title: "Trang chủ", input: "https://www.hentaivnx.vip", script: "gen.js"},
+        {title: "Truyện mới", input: "https://www.hentaivnx.vip/tim-truyen", script: "gen.js"},
+        {title: "Không che", input: "https://www.hentaivnx.vip/tim-truyen/khong-che", script: "gen.js"},
+        {title: "Truyện màu", input: "https://www.hentaivnx.vip/tim-truyen/truyen-mau", script: "gen.js"},
+        {title: "Series", input: "https://www.hentaivnx.vip/tim-truyen/series", script: "gen.js"}
     ]);
 }
